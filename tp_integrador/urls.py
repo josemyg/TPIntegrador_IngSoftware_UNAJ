901df3d053ebb726edceb2f7747f087aa9f1cde7
AxelPradela <axelpadela03@gmail.com>
2026-05-31 13:06:21 -0300

    actualizo urls y settings

--- tp_integrador/urls.py
+++ tp_integrador/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gestion.urls')),
+    path('descuentos/', include('descuentos.urls')),
 ]
 
 
