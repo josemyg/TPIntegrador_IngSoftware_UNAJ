0164b88ff07bbb12700e5fe82d0864d20bc2f3c9
AxelPradela <axelpadela03@gmail.com>
2026-06-04 01:47:26 -0300

    tipo de descuento agregado y ver certificado de profesor

--- descuentos/forms.py
+++ descuentos/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.db.models import Subquery, OuterRef
-from .models import  Descuento
+from .models import  Descuento, TipoDescuento
 
 class DescuentoForm(forms.ModelForm):
     class Meta:
@@ -12,4 +12,17 @@ class DescuentoForm(forms.ModelForm):
         widgets = {
             'tipo': forms.Select(attrs={'class':'form-select'}),
             'cantidad': forms.NumberInput(attrs={'class':'form-control'}),
+        }
+
+
+class TipoDescuentoForm(forms.ModelForm):
+    class Meta:
+        model=TipoDescuento
+        template_name='gestion/TipoDescuento/TipoDescuento_form.html'
+        fields = '__all__'
+        exclude = ['estado','user_django']
+
+
+        widgets = {
+            'nombreDescuento': forms.TextInput(attrs={'class':'form-control'}),
         }
--- descuentos/models.py
+++ descuentos/models.py
@@ -5,39 +5,58 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_delete
 
 # Create your models here.
+
+
+
+class TipoDescuento(models.Model):
+
+    nombreDescuento = models.CharField(
+        "Tipo de descuento",
+        max_length=50
+    )
+
+    def __str__(self):
+        return self.nombreDescuento
+
+
 class Descuento(models.Model):
 
-
-    tiposDescuentos=[
-        ('socio', 'Socio del club'),
-        ('empleado', 'Empleado'),
-        ('temporal', 'Promoción temporal'),
-        ('torneo', 'Torneo'),
-    ]
-
     estados = [
-    ('activo', 'Activo'),
-    ('programado', 'Programado'),
-    ('vencido', 'Vencido'),
-    ('suspendido', 'Suspendido'),
-    ('baja', 'Baja'),
+        ('activo', 'Activo'),
+        ('programado', 'Programado'),
+        ('vencido', 'Vencido'),
+        ('suspendido', 'Suspendido'),
+        ('baja', 'Baja'),
     ]
 
-    tipo = models.CharField(("Tipo"), choices=tiposDescuentos, max_length=50)
+    tipo = models.ForeignKey(
+        TipoDescuento,
+        on_delete=models.PROTECT
+    )
 
-    cantidad=models.DecimalField(max_digits=5, decimal_places=2)
+    cantidad = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
 
-    estado = models.CharField(("Estado"), choices=estados,default="activo",max_length=50)
-
+    estado = models.CharField(
+        "Estado",
+        choices=estados,
+        default="activo",
+        max_length=50
+    )
 
     class Meta:
-        verbose_name = ("Descuento")
-        verbose_name_plural = ("Descuentos")
-
+        verbose_name = "Descuento"
+        verbose_name_plural = "Descuentos"
 
     def descuento_Activo(self):
-        return self.estado=="activo"
+        return self.estado == "activo"
 
     def darDescuento_Baja(self):
-        self.estado="baja"
-        self.save()    
+        self.estado = "baja"
+        self.save()
+
+
+        
+        
